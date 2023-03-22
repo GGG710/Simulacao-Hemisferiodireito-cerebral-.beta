bbde36ae208b04b2c99722e3b9bb3eabc5d3bb9d
GGG <127780046+GGG710@users.noreply.github.com>
2023-03-22 16:50:20 +0000

    Pending changes exported from your codespace

---
+++ Hemisferiodireito.py
@@ -0,0 +1,22 @@
+print("Olá,bem-vindo(a) ao cérebro:hemisfério direito") 
+print("(''Aviso,todos os códigos rodarão de uma vez'')")      
+input("Arte,3D ou criatividade?:")
+
+if input :"Arte"
+print("''Poema:É sempre mais difícil ancorar um navio no espaço''-(Ana Cristina César)")
+
+if input:"3d"
+print("Exemplo de um cubo=🧊")
+
+if input:"criatividade"
+from random import choice
+import string
+print("Hora das ideias! ")
+tamanho = input("Qual será o tamanho da sua nova palavra?: ")
+tamanho = int(tamanho)
+valores = string.ascii_letters + string.digits
+palavra = ''
+for i in range(tamanho):
+    palavra += choice(valores)
+
+print("Aqui está a sua nova palavra!: " + palavra)
